cdb3809ea0b23fc73bd9bdd517c90400e2fd37c7
Alexandre <allespag@student.42.fr>
2022-07-25 20:30:09 +0200

    delete stuff

--- rubikapp/static/js-OLD/utils.js
+++
@@ -1,9 +0,0 @@
-function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
-}
-
-function getRandomKey(collection) {
-    let keys = Array.from(collection.keys());
-    let index = Math.floor(Math.random() * keys.length);
-    return keys[index];
-}
