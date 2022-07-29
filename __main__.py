e1c59395ee1bbbc11cd1da472fb2e9fbf36ce8bf
Alexandre <allespag@student.42.fr>
2022-07-29 15:46:44 +0200

    added the --perf flag

--- __main__.py
+++ __main__.py
@@ -1,4 +1,6 @@
 import argparse
+import cProfile
+import pstats
 import sys
 
 from rubik.cube import Cube
@@ -15,15 +17,27 @@ from rubik.solver import Solver
 # 5.73s (from __main__.py)
 # 5.42s (je sais pas pourquoi)
 # 2.90s (remove path_set)
+# 2.50s (en utilisant une espèce de priorityqueue)
 # B' L D2 R' F' F L' U2 L B2 B' B R2 B' R' B' F' R U F' D2 B U R D' F2 F2 U' B2 U2 L' D2 R B B' F D2 F U' D L B'
 
 # 8.68s
+# 7.49s (je sais pas pourquoi)
+# 0.31s (en utilisant une espèce de priorityqueue)
 # L D' B U B' D D2 F' L U2 F' U2 B2 R' D2 U F' L' R2 U B' R2 R U' F' L' B2 D F2 B2 D2 B' U2 R' F' F F2 U' D' F2 R R'
 
-__CHECK_PERF = False
-if __CHECK_PERF:
-    import cProfile
-    import pstats
+# plus lent avec benchmark -> n = 1000 ; seed = 42
+# 4.91s (from benchmark)
+# 2.78s (en utilisant une espèce de priorityqueue)
+# L' F' B' R2 D' F R L2 U2 L B2 R R' D' F' D B' L2 U2 U' U D D' D2 R2 U F2 R B B2 U' R R D2 D U2 D R' U' D' F' B2
+
+
+# plus lent avec benchmark -> n = 500 ; seed = 42
+# 4.39s (from benchmark)
+# R L L R2 F2 R' D2 U2 F2 R' U R' R' U' F2 U2 D' F2 U U' R F' L R2 F D D B D' F B B' L2 B F2 U' D2 B' D2 D2 F2 D
+
+# plus lent avec benchmark -> n = 1000 ; seed = 42
+# 5.15s (from benchmark)
+# "L' F' B' R2 D' F R L2 U2 L B2 R R' D' F' D B' L2 U2 U' U D D' D2 R2 U F2 R B B2 U' R R D2 D U2 D R' U' D' F' B2
 
 
 def main(args: argparse.Namespace) -> None:
@@ -43,7 +57,7 @@ def main(args: argparse.Namespace) -> None:
 
     print(f"\nSolving: {sequence_to_readable(sequence)}")
 
-    if __CHECK_PERF:
+    if args.perf:
         profile = cProfile.Profile()
         solution = profile.runcall(solver.run)
         ps = pstats.Stats(profile)
@@ -61,6 +75,8 @@ def get_args() -> argparse.Namespace:
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("sequence", nargs="?")
     group.add_argument("--random", "-r", type=int)
+
+    parser.add_argument("--perf", "-p", action="store_true", default=False)
 
     args = parser.parse_args()
     return args
